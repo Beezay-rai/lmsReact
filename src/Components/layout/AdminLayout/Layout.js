3b45dc94e328f6ec583e93ffe6681483260f24cc
Yuki - rei <108078995+Yuki-rai@users.noreply.github.com>
2024-02-01 18:16:16 +0545

    Side bar open close erroe

--- src/Components/layout/AdminLayout/Layout.js
+++ src/Components/layout/AdminLayout/Layout.js
@@ -1,90 +1,80 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Sidebar from './Sidebar';
-import { AppBar, Avatar, Badge, Button, Collapse, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from '@mui/material';
-import { IoMdNotificationsOutline } from 'react-icons/io'
-import { useState } from 'react';
-import ava from '../../../assests/img/ava.jpg'
-import { MdExpandLess, MdExpandMore } from 'react-icons/md';
-import { Link } from 'react-router-dom';
-import { PiChatDotsDuotone } from 'react-icons/pi';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Sidebar from "./Sidebar";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Button,
+  Collapse,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import ava from "../../../assests/img/ava.jpg";
+import { MdExpandLess, MdExpandMore } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { PiChatDotsDuotone } from "react-icons/pi";
+import MenuIcon from "@mui/icons-material/Menu";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+
 export default function Layout({ children }) {
+  const [isNotificationOpen, setIsNotificationOpen] = useState(false);
+  const [drawerOpen,setDrawerOpen] = useState(false);
 
-    const [isNotificationOpen, setIsNotificationOpen] = useState(false);
-
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-                <Toolbar sx={{ justifyContent: 'space-between' }}>
-                    <Typography variant="h6" noWrap component="div">
-                        LMS
-                    </Typography>
-                    <div className='flex '>
-                        <List>
-                            <ListItemButton sx={{ padding: "5px" }} onClick={() => setIsNotificationOpen(!isNotificationOpen)}>
-                                <Badge badgeContent={5} color="error">
-                                    <IoMdNotificationsOutline size={25}></IoMdNotificationsOutline>
-
-                                </Badge>
-                                {isNotificationOpen ? <MdExpandMore /> : <MdExpandLess className='' />}
-                            </ListItemButton>
-                            <Collapse in={isNotificationOpen} className='absolute w-40 rounded-xl bg-white -translate-x-28'>
-                                <div className='notification p-3'>
-                                    <div className='notification-header'>
-                                        <h4 className='text-black'>Header</h4>
-                                    </div>
-                                    <div className='notification-body'>
-                                        <p className='text-slate-400 font-serif text-sm overflow-hidden  max-w-full'>
-                                            Hy  my name is bijay rai and i dont know tg
-                                        </p>
-                                    </div>
-                                </div>
-                                <Divider/>
-                                <List disablePadding>
-                                    <ListItem disablePadding >
-                                        <ListItemButton>
-                                            <ListItemIcon >
-                                            <PiChatDotsDuotone/>
-                                            </ListItemIcon>
-                                            <ListItemText className='text-slate-400 font-serif text-sm overflow-hidden  max-w-full'>
-                                                asd
-                                                Hy  my name is bijay rai and i dont know tg
-                                            </ListItemText>
-                                        </ListItemButton>
-                                    </ListItem>
-                                    <ListItem disablePadding >
-                                        <ListItemButton>
-                                            <ListItemIcon >
-                                            <PiChatDotsDuotone/>
-                                            </ListItemIcon>
-                                            <ListItemText className='text-black'>
-                                                asd
-                                            </ListItemText>
-                                        </ListItemButton>
-                                    </ListItem>
-                                </List>
+  const handleSideBar = ()=>{
+    
+    setDrawerOpen(!drawerOpen)
+  }
 
 
+  return (
+    <>
+      <AppBar sx={{zIndex:(theme)=>theme.zIndex.drawer + 1 }} >
+        <Toolbar className="j justify-between">
+          <Box className="flex ">
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+              onClick={handleSideBar}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">LMS</Typography>
+          </Box>
 
-                            </Collapse>
+          <Box>
+            <IconButton>
+              <Badge badgeContent={4} color="error">
+                <NotificationsIcon />
+              </Badge>
+            </IconButton>
 
-                        </List>
+            <IconButton>
+              <Avatar src={ava}></Avatar>
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
 
-
-                        <div className='pt-1' >
-                            <Button>
-                                <Avatar src={ava}></Avatar>
-                            </Button>
-                        </div>
-
-                    </div>
-                </Toolbar>
-            </AppBar>
-
-            <Sidebar />
-            <Box component="main" sx={{ width: `100%`, margin: '80px 30px 30px 30px ', padding: `0px` }}>
-                {children}
-            </Box>
-        </Box>
-    );
+      <Sidebar sideBarOpen={drawerOpen} />
+      <Box
+        component="main"
+      >
+        {children}
+      </Box>
+    </>
+  );
 }
--- src/Components/layout/AdminLayout/Sidebar.js
+++ src/Components/layout/AdminLayout/Sidebar.js
@@ -1,243 +1,228 @@
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import { AiOutlineSetting, AiOutlineUserAdd } from 'react-icons/ai'
-import { FaHome, FaWrench } from 'react-icons/fa'
-import { PiStudentFill } from 'react-icons/pi'
-import { GiBookCover } from 'react-icons/gi'
-import { MdBook, MdExpandLess, MdExpandMore } from 'react-icons/md'
-import { FiLogOut } from 'react-icons/fi'
-import { Link } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { Button, Collapse, Modal } from '@mui/material';
-import { useDispatch } from 'react-redux';
-import { logout } from '../../../redux/userDetail';
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import { AiOutlineSetting, AiOutlineUserAdd } from "react-icons/ai";
+import { FaHome, FaWrench } from "react-icons/fa";
+import { PiStudentFill } from "react-icons/pi";
+import { GiBookCover } from "react-icons/gi";
+import { MdBook, MdExpandLess, MdExpandMore } from "react-icons/md";
+import { FiLogOut } from "react-icons/fi";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Button, Collapse, Modal } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { logout } from "../../../redux/userDetail";
+import zIndex from "@mui/material/styles/zIndex";
 
-const drawerWidth = 240;
-function Sidebar(props) {
+function Sidebar({ sideBarOpen }) {
+  const modalStyle = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "white",
+    border: "1px solid silver",
+    outline: "none",
+    p: 4,
+    padding: "20px",
+  };
 
-    const modalStyle = {
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        width: 400,
-        bgcolor: 'white',
-        border: '1px solid silver',
-        outline: 'none',
-        p: 4,
-        padding: '20px'
-    };
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    
-    //Login Modal
-    const [modalOpen, setModalOpen] = useState(false);
+  //Login Modal
+  const [modalOpen, setModalOpen] = useState(false);
 
-    //SetUp Dropdown
-    const [open, setOpen] = useState(false);
-    const handleListItem = () => {
-        setOpen(!open);
-    }
-    const handleModalOpen = () => {
-        setModalOpen(!modalOpen);
-    }
-    // for Main Setup Dropdown
-    const [mainListOpen,setMainListOPen]= useState(false);
-    const handleMainListOpen = ()=>{
-        setMainListOPen(!mainListOpen)
-    }
+  //SetUp Dropdown
+  const [open, setOpen] = useState(false);
+  const handleListItem = () => {
+    setOpen(!open);
+  };
+  const handleModalOpen = () => {
+    setModalOpen(!modalOpen);
+  };
+  // for Main Setup Dropdown
+  const [mainListOpen, setMainListOPen] = useState(false);
+  const handleMainListOpen = () => {
+    setMainListOPen(!mainListOpen);
+  };
+  const drawerWidth = 250;
+  return (
+    <>
+      <Drawer
+        variant="persistent"
+        sx={{
+          width: drawerWidth,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        open={sideBarOpen}
+      >
+        <List>
+          <Link to="/">
+            <ListItemButton>
+              <ListItemIcon>
+                <FaHome />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </Link>
+          {/* Main Set Up */}
+          <ListItemButton onClick={handleMainListOpen}>
+            <ListItemIcon>
+              <FaWrench />
+            </ListItemIcon>
+            <ListItemText primary="Main SetUp" />
+            {mainListOpen ? <MdExpandLess /> : <MdExpandMore />}
+          </ListItemButton>
+          <Collapse in={mainListOpen}>
+            <List component="div" disablePadding>
+              {mainSetUpArray.map(({ label, icon, to }) => {
+                return (
+                  <ListItem key={label} disablePadding>
+                    <Link
+                      to={to}
+                      style={{
+                        width: "100%",
+                      }}
+                    >
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        <ListItemText primary={label} />
+                      </ListItemButton>
+                    </Link>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Collapse>
 
-    return (
-        <div className='hidden xl:block '>
+          {/* Set Up */}
+          <ListItemButton onClick={handleListItem}>
+            <ListItemIcon>
+              <FaWrench />
+            </ListItemIcon>
+            <ListItemText primary="SetUp" />
+            {open ? <MdExpandLess /> : <MdExpandMore />}
+          </ListItemButton>
+          <Collapse in={open}>
+            <List component="div" disablePadding>
+              {setUpArray.map(({ label, icon, to }) => {
+                return (
+                  <ListItem key={label} disablePadding>
+                    <Link to={to} style={{ width: "100%" }}>
+                      <ListItemButton sx={{ pl: 4 }}>
+                        <ListItemIcon>{icon}</ListItemIcon>
+                        <ListItemText primary={label} />
+                      </ListItemButton>
+                    </Link>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </Collapse>
+          <Link
+            to="/Admin/Transaction"
+            style={{
+              width: "100%",
+            }}
+          >
+            <ListItemButton>
+              <ListItemIcon>
+                <MdBook />
+              </ListItemIcon>
+              <ListItemText primary="Transaction" />
+            </ListItemButton>
+          </Link>
+        </List>
+        <Divider />
 
-            <Drawer
-                variant="permanent"
-                sx={{
-                    width: drawerWidth,
-                    flexShrink: 0,
-                    [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-                }}
+        <List>
+          <ListItemButton>
+            <ListItemIcon>
+              <AiOutlineSetting />
+            </ListItemIcon>
+            <ListItemText primary="Setting" />
+          </ListItemButton>
 
-            >
-                <Toolbar />
-                <Box sx={{ overflow: 'hidden' }}>
-                    <List>
-                        <Link to="/" style={{
-                            color: 'inherit',
-                            textDecoration: 'none',
-                            width: '100%'
-                        }}>
-                            <ListItemButton sx={{ padding: '15px' }} >
-                                <ListItemIcon sx={{ fontSize: '25px' }}>
-                                    <FaHome />
-                                </ListItemIcon>
-                                <ListItemText primary="Home" />
-                            </ListItemButton>
-                        </Link>
-                        {/* Main Set Up */}
-                        <ListItemButton sx={{ padding: '15px' }} onClick={handleMainListOpen}>
-                            <ListItemIcon sx={{ fontSize: '25px' }}>
-                                <FaWrench />
-                            </ListItemIcon>
-                            <ListItemText primary="Main SetUp" />
-                            {mainListOpen ? <MdExpandLess /> : <MdExpandMore />}
-                        </ListItemButton>
-                        <Collapse in={mainListOpen} >
-                        <List sx={{ padding: `0px 0px 0px 25px` }}>
-                                {mainSetUpArray.map(({ label, icon, to }) => {
-                                    return (
-                                        <ListItem key={label} disablePadding>
-                                            <Link to={to} style={{
-                                                color: 'inherit',
-                                                textDecoration: 'none',
-                                                width: '100%'
-                                            }}>
-                                                <ListItemButton sx={{ padding: '15px' }}>
-                                                    <ListItemIcon sx={{ fontSize: '25px' }}>
-                                                        {icon}
-                                                    </ListItemIcon>
-                                                    <ListItemText primary={label} />
-                                                </ListItemButton>
-                                            </Link>
-                                        </ListItem>
-                                    )
-                                })}
-                            </List>
-                        </Collapse>
+          <ListItemButton onClick={handleModalOpen}>
+            <ListItemIcon>
+              <FiLogOut />
+            </ListItemIcon>
+            <ListItemText primary="Log out" />
+          </ListItemButton>
+        </List>
+      </Drawer>
 
-                        {/* Set Up */}
-                        <ListItemButton sx={{ padding: '15px' }} onClick={handleListItem}>
-                            <ListItemIcon sx={{ fontSize: '25px' }}>
-                                <FaWrench />
-                            </ListItemIcon>
-                            <ListItemText primary="SetUp" />
-                            {open ? <MdExpandLess /> : <MdExpandMore />}
-                        </ListItemButton>
-                        <Collapse in={open}>
-                            <List sx={{ padding: `0px 0px 0px 25px` }}>
-                                {setUpArray.map(({ label, icon, to }) => {
-                                    return (
-                                        <ListItem key={label} disablePadding>
-                                            <Link to={to} style={{
-                                                color: 'inherit',
-                                                textDecoration: 'none',
-                                                width: '100%'
-                                            }}>
-                                                <ListItemButton sx={{ padding: '15px' }}>
-                                                    <ListItemIcon sx={{ fontSize: '25px' }}>
-                                                        {icon}
-                                                    </ListItemIcon>
-                                                    <ListItemText primary={label} />
-                                                </ListItemButton>
-                                            </Link>
-                                        </ListItem>
-                                    )
-                                })}
-                            </List>
-                        </Collapse>
-                        <Link to="/Admin/Transaction" style={{
-                            color: 'inherit',
-                            textDecoration: 'none',
-                            width: '100%'
-                        }}>
-                            <ListItemButton sx={{ padding: '15px' }} >
-                                <ListItemIcon sx={{ fontSize: '25px' }}>
-                                    <MdBook />
-                                </ListItemIcon>
-                                <ListItemText primary="Transaction" />
-                            </ListItemButton>
-                        </Link>
-                    </List>
-                    <Divider />
-                    <List>
-
-                        <ListItemButton sx={{ padding: '15px' }} >
-                            <ListItemIcon sx={{ fontSize: '25px' }}>
-                                <AiOutlineSetting />
-                            </ListItemIcon>
-                            <ListItemText primary="Setting" />
-                        </ListItemButton>
-
-                        <ListItemButton sx={{ padding: '15px' }} onClick={handleModalOpen}  >
-                            <ListItemIcon sx={{ fontSize: '25px' }}>
-                                <FiLogOut />
-                            </ListItemIcon>
-                            <ListItemText primary="Log out" />
-                        </ListItemButton>
-                    </List>
-                </Box>
-            </Drawer>
-
-            {/* Log Out Modal Pop up */}
-            <Modal
-                open={modalOpen}
-                onClose={handleModalOpen}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-
-            >
-                <Box sx={modalStyle} className='rounded-2xl'>
-                    <Typography id="modal-modal-title" variant="h6" component="h2" sx={{ padding: `20px`, textAlign: `center` }}>
-                        Are you sure,<br /> you want to log out?
-                    </Typography>
-                    <Toolbar sx={{ justifyContent: 'space-evenly' }}>
-                        <Button variant="contained" onClick={() => dispatch(logout())}>Log Out</Button>
-                        <Button variant="outlined" color='error' onClick={handleModalOpen}>Cancel</Button>
-                    </Toolbar>
-
-                </Box>
-            </Modal>
-            {/* Modal popup End */}
-
-        </div >
-    )
+      {/* Log Out Modal Pop up */}
+      <Modal
+        open={modalOpen}
+        onClose={handleModalOpen}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={modalStyle} className="rounded-2xl">
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+            component="h2"
+            sx={{ padding: `20px`, textAlign: `center` }}
+          >
+            Are you sure,
+            <br /> you want to log out?
+          </Typography>
+          <Toolbar sx={{ justifyContent: "space-evenly" }}>
+            <Button variant="contained" onClick={() => dispatch(logout())}>
+              Log Out
+            </Button>
+            <Button variant="outlined" color="error" onClick={handleModalOpen}>
+              Cancel
+            </Button>
+          </Toolbar>
+        </Box>
+      </Modal>
+      {/* Modal popup End */}
+    </>
+  );
 }
 
-
-
-
 const setUpArray = [
-
-    {
-        label: "Course",
-        icon: <AiOutlineUserAdd />,
-        to: "/Admin/Course",
-    },
-    {
-        label: "Student",
-        icon: <PiStudentFill />,
-        to: "/Admin/Student",
-    },
-    {
-        label: "Book",
-        icon: <GiBookCover />,
-        to: "/Admin/Book",
-
-
-    },
-    {
-        label: "Category",
-        icon: <MdBook />,
-        to: "/Admin/Category",
-
-    }
-]
+  {
+    label: "Course",
+    icon: <AiOutlineUserAdd />,
+    to: "/Admin/Course",
+  },
+  {
+    label: "Student",
+    icon: <PiStudentFill />,
+    to: "/Admin/Student",
+  },
+  {
+    label: "Book",
+    icon: <GiBookCover />,
+    to: "/Admin/Book",
+  },
+  {
+    label: "Category",
+    icon: <MdBook />,
+    to: "/Admin/Category",
+  },
+];
 
 const mainSetUpArray = [
-    {
-        label:"User",
-        icon: <AiOutlineUserAdd />,
-        to:"/Admin/UserList"
-    }
-]
+  {
+    label: "User",
+    icon: <AiOutlineUserAdd />,
+    to: "/Admin/UserList",
+  },
+];
 
-export default Sidebar
-
+export default Sidebar;
